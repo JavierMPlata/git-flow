64db3baa7f0f2eeb6712ff0560adf29c4bbc1749
JavierMPlata <jmplatap@academia.usbbog.edu.co>
2025-08-11 18:33:11 -0500

    Release: add a new comment

--- git-flow.py
+++ git-flow.py
@@ -2,6 +2,6 @@ print("Hello, World!")
 # Esto es una función que imprime 'Hello, World!'
 def hello_world():
     print("Hello, World!")
-
+# Esto es una condición que verifica si el script se está ejecutando directamente
 if __name__ == "__main__":
     hello_world()
