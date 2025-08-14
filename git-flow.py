71f1c02b5c05fa14ab44888d8fc3b9a904e40d1a
JavierMPlata <jmplatap@academia.usbbog.edu.co>
2025-08-13 19:47:18 -0500

    feat: Nueva linea de codigo para saber si es mayor o menor de edad, en fase de lanzamiento

--- git-flow.py
+++ git-flow.py
@@ -5,3 +5,10 @@ def hello_world():
 # Esto es una condición que verifica si el script se está ejecutando directamente
 if __name__ == "__main__":
     hello_world()
+
+    # Solicita la edad al usuario y verifica si es mayor o menor de edad
+    edad = int(input("Introduce tu edad: "))
+    if edad >= 18:
+        print("Eres mayor de edad.")
+    else:
+        print("Eres menor de edad.")
